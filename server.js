cd6e3f64053d670ae72271daf41cdd4cb2abb2eb
mrtlckn <ahmet.telceken@hotmail.com>
2021-12-31 15:14:27 +0300

    xyz

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 const mongoose = require("mongoose");
 
 var cors = require('cors')
+
 app.use(cors())
+
 
 
 mongoose.connect(
@@ -24,7 +26,13 @@ const usersRouter = require("./routes/user");
 app.use(cors(), (req, res, next) => {
   next();
 });
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 app.use("/api/users", usersRouter);
 
