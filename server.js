2269e96be33077a7c014e8439fc0e749b8e1b1cd
mrtlckn <ahmet.telceken@hotmail.com>
2021-12-31 15:20:40 +0300

    public

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ var cors = require('cors')
 
 app.use(cors())
 
-
-
 mongoose.connect(
   "mongodb+srv://admin:12345671@cluster0.jrvo4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   { useNewUrlParser: true }
@@ -26,14 +24,7 @@ const usersRouter = require("./routes/user");
 app.use(cors(), (req, res, next) => {
   next();
 });
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
 
 app.use("/api/users", usersRouter);
 
-app.listen(7000, () => console.log("Server Started"));
+app.listen(process.env.PORT || 8080, () => console.log("Server Started"));
